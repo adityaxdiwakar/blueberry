0c5bc76aa3ce7597b40ab4cdeebdc0526a376b8a
Aditya Diwakar <aditya@diwakar.io>
2020-04-26 22:24:07 -0400

    Cleaned up code

--- src/main.go
+++ src/main.go
@@ -1,35 +1,36 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
 	"os/signal"
-	"time"
 	"strings"
-	"bytes"
-	"io/ioutil"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 
 	"database/sql"
-	 _ "github.com/go-sql-driver/mysql"
-	 
+
+	_ "github.com/go-sql-driver/mysql"
+
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type PriceWithSize struct {
+type priceWithSize struct {
 	Price float64 `json:"price"`
-	Size int `json:"size"`
+	Size  int     `json:"size"`
 }
 
-type ResponseObject []struct {
+type responseObjectStruct []struct {
 	E string `json:"e"`
 	D struct {
 		Quotes []struct {
@@ -37,9 +38,9 @@ type ResponseObject []struct {
 			Timestamp  time.Time `json:"timestamp"`
 			ContractID int       `json:"contractId"`
 			Entries    struct {
-				Bid PriceWithSize `json:"Bid"`
-				Offer PriceWithSize `json:"Offer"`
-				Trade PriceWithSize `json:"Trade"`
+				Bid              priceWithSize `json:"Bid"`
+				Offer            priceWithSize `json:"Offer"`
+				Trade            priceWithSize `json:"Trade"`
 				TotalTradeVolume struct {
 					Size int `json:"size"`
 				} `json:"TotalTradeVolume"`
@@ -64,42 +65,42 @@ type ResponseObject []struct {
 	} `json:"d"`
 }
 
-type SessionObject struct {
-	OpeningPrice float64 `json:"open"`
-	HighPrice float64 `json:"high"`
+type sessionObject struct {
+	OpeningPrice    float64 `json:"open"`
+	HighPrice       float64 `json:"high"`
 	SettlementPrice float64 `json:"settlement"`
-	LowPrice float64 `json:"low"`
-}  
-
-type DepthObject struct {
-	Bid PriceWithSize `json:"bid"`
-	Ask PriceWithSize `json:"ask"`
+	LowPrice        float64 `json:"low"`
 }
 
-type SQLQuoteObject struct {
-	ID int `json:"id"`
-	ContractID int `json:"contract_id"`
-	SessionVolume int `json:"session_volume"`
-	OpenInterest int `json:"open_interest"`
-	OpeningPrice float64 `json:"opening_price"`
-	HighPrice float64 `json""high_price"`
+type depthObject struct {
+	Bid priceWithSize `json:"bid"`
+	Ask priceWithSize `json:"ask"`
+}
+
+type sqlQuoteObject struct {
+	ID              int     `json:"id"`
+	ContractID      int     `json:"contract_id"`
+	SessionVolume   int     `json:"session_volume"`
+	OpenInterest    int     `json:"open_interest"`
+	OpeningPrice    float64 `json:"opening_price"`
+	HighPrice       float64 `json:"high_price"`
 	SettlementPrice float64 `json:"settlement_price"`
-	LowPrice float64 `json:"low_price"`
-	BidPrice float64 `json:"bid_price"`
-	BidSize int `json:"bid_size"`
-	AskPrice float64 `json:"ask_price"`
-	AskSize int `json:"ask_size"`
-	TradePrice float64 `json:"trade_price"`
-	TradeSize int `json:"trade_size"`
-	Timestamp int64 `json:"timestamp"`
+	LowPrice        float64 `json:"low_price"`
+	BidPrice        float64 `json:"bid_price"`
+	BidSize         int     `json:"bid_size"`
+	AskPrice        float64 `json:"ask_price"`
+	AskSize         int     `json:"ask_size"`
+	TradePrice      float64 `json:"trade_price"`
+	TradeSize       int     `json:"trade_size"`
+	Timestamp       int64   `json:"timestamp"`
 }
 
-type TradovateCredentials struct {
+type tradovateCredentials struct {
 	Username string `json:"name"`
 	Password string `json:"password"`
 }
 
-type TradovateKeyResponse struct {
+type tradovateKeyResponse struct {
 	AccessToken    string    `json:"accessToken"`
 	MdAccessToken  string    `json:"mdAccessToken"`
 	ExpirationTime time.Time `json:"expirationTime"`
@@ -113,7 +114,7 @@ type TradovateKeyResponse struct {
 }
 
 func getAccessToken(username string, password string) string {
-	payload := TradovateCredentials{
+	payload := tradovateCredentials{
 		Username: username,
 		Password: password,
 	}
@@ -123,21 +124,20 @@ func getAccessToken(username string, password string) string {
 	}
 	req, err := http.NewRequest("POST", "https://live-api.tradovate.com/v1/auth/accesstokenrequest", bytes.NewBuffer(payloadByteArray))
 	client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        panic(err)
-    }
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
 	defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)	
-	respObj := TradovateKeyResponse{}
+	body, _ := ioutil.ReadAll(resp.Body)
+	respObj := tradovateKeyResponse{}
 	json.Unmarshal([]byte(string(body)), &respObj)
 	log.Printf("Retrieved Access Token with Credentials")
 	return respObj.MdAccessToken
 }
 
 var addr = flag.String("addr", "md-api.tradovate.com", "http service address")
-var quoteObject QuoteObject;
-
+var quoteObject quoteObjectStruct
 
 var db *sql.DB
 
@@ -152,39 +152,39 @@ func initCache() {
 	if err != nil {
 		log.Fatal("Error ocurred! Could not populate cache!")
 	}
-	
+
 	for results.Next() {
-		cacheObject := SQLQuoteObject{}
+		cacheObject := sqlQuoteObject{}
 		err = results.Scan(&cacheObject.ID, &cacheObject.ContractID, &cacheObject.SessionVolume, &cacheObject.OpenInterest, &cacheObject.OpeningPrice, &cacheObject.HighPrice, &cacheObject.SettlementPrice, &cacheObject.LowPrice,
-						   &cacheObject.BidPrice, &cacheObject.BidSize, &cacheObject.AskPrice, &cacheObject.AskSize, &cacheObject.TradePrice, &cacheObject.TradeSize, &cacheObject.Timestamp)
+			&cacheObject.BidPrice, &cacheObject.BidSize, &cacheObject.AskPrice, &cacheObject.AskSize, &cacheObject.TradePrice, &cacheObject.TradeSize, &cacheObject.Timestamp)
 		if err != nil {
 			log.Fatal("Error occured population object", err)
 		}
 
-		quoteObject = QuoteObject{
+		quoteObject = quoteObjectStruct{
 			Timestamp:     cacheObject.Timestamp,
 			ContractID:    cacheObject.ContractID,
 			SessionVolume: cacheObject.SessionVolume,
 			OpenInterest:  cacheObject.OpenInterest,
-			SessionPrices: SessionObject{
+			SessionPrices: sessionObject{
 				OpeningPrice:    cacheObject.OpeningPrice,
 				HighPrice:       cacheObject.HighPrice,
 				SettlementPrice: cacheObject.SettlementPrice,
 				LowPrice:        cacheObject.LowPrice,
 			},
-			Depth: DepthObject{
-				Bid: PriceWithSize{
+			Depth: depthObject{
+				Bid: priceWithSize{
 					Price: cacheObject.BidPrice,
-					Size: cacheObject.BidSize,
+					Size:  cacheObject.BidSize,
 				},
-				Ask: PriceWithSize{
+				Ask: priceWithSize{
 					Price: cacheObject.AskPrice,
-					Size: cacheObject.AskSize,
+					Size:  cacheObject.AskSize,
 				},
 			},
-			Trade: PriceWithSize{
+			Trade: priceWithSize{
 				Price: cacheObject.TradePrice,
-				Size: cacheObject.TradeSize,
+				Size:  cacheObject.TradeSize,
 			},
 		}
 	}
@@ -227,10 +227,10 @@ func main() {
 
 	username := os.Getenv("USERNAME")
 	password := os.Getenv("PASSWORD")
-	accessToken := getAccessToken(username, password)		
+	accessToken := getAccessToken(username, password)
 
 	//establish a connection
-	c.WriteMessage(websocket.TextMessage, []byte("authorize\n2\n\n" + accessToken))
+	c.WriteMessage(websocket.TextMessage, []byte("authorize\n2\n\n"+accessToken))
 
 	done := make(chan struct{})
 
@@ -243,40 +243,40 @@ func main() {
 				log.Fatal("read:", err)
 				return
 			}
-			count += 1
-			if (count == 2) {
+			count++
+			if count == 2 {
 				c.WriteMessage(websocket.TextMessage, []byte("md/subscribequote\n7\n\n{\"symbol\":1717695}"))
 			}
 
-			r_message := string(message)
+			rMessage := string(message)
 			switch {
-			case strings.Contains(r_message, "quotes"):
+			case strings.Contains(rMessage, "quotes"):
 				log.Printf("Quote received")
-				r_message = r_message[1:]
-				data := ResponseObject{}
-				json.Unmarshal([]byte(r_message), &data)
+				rMessage = rMessage[1:]
+				data := responseObjectStruct{}
+				json.Unmarshal([]byte(rMessage), &data)
 				compressedResponse := data[0].D.Quotes[0]
-				quoteObject = QuoteObject{
-					Timestamp:     compressedResponse.Timestamp.UnixNano()/(1000*1000),
+				quoteObject = quoteObjectStruct{
+					Timestamp:     compressedResponse.Timestamp.UnixNano() / (1000 * 1000),
 					ContractID:    compressedResponse.ContractID,
 					SessionVolume: compressedResponse.Entries.TotalTradeVolume.Size,
 					OpenInterest:  compressedResponse.Entries.OpenInterest.Size,
-					SessionPrices: SessionObject{
+					SessionPrices: sessionObject{
 						OpeningPrice:    compressedResponse.Entries.OpeningPrice.Price,
 						HighPrice:       compressedResponse.Entries.HighPrice.Price,
 						SettlementPrice: compressedResponse.Entries.SettlementPrice.Price,
 						LowPrice:        compressedResponse.Entries.LowPrice.Price,
 					},
-					Depth: DepthObject{
+					Depth: depthObject{
 						Bid: compressedResponse.Entries.Bid,
 						Ask: compressedResponse.Entries.Offer,
 					},
 					Trade: compressedResponse.Entries.Trade,
 				}
-				statement := fmt.Sprintf("insert into quotes (contract_id, session_volume, open_interest, opening_price, high_price, settlement_price, low_price, bid_price, bid_size, ask_price, ask_size, trade_price, trade_size, timestamp) VALUES (%d, %d, %d, %f, %f, %f, %f, %f, %d, %f, %d, %f, %d, %d)", 
-										compressedResponse.ContractID, compressedResponse.Entries.TotalTradeVolume.Size, compressedResponse.Entries.OpenInterest.Size, compressedResponse.Entries.OpeningPrice.Price, compressedResponse.Entries.HighPrice.Price, compressedResponse.Entries.SettlementPrice.Price,
-										compressedResponse.Entries.LowPrice.Price, compressedResponse.Entries.Bid.Price, compressedResponse.Entries.Bid.Size, compressedResponse.Entries.Offer.Price, compressedResponse.Entries.Offer.Size, compressedResponse.Entries.Trade.Price, compressedResponse.Entries.Trade.Size,
-										compressedResponse.Timestamp.UnixNano()/(1000*1000))
+				statement := fmt.Sprintf("insert into quotes (contract_id, session_volume, open_interest, opening_price, high_price, settlement_price, low_price, bid_price, bid_size, ask_price, ask_size, trade_price, trade_size, timestamp) VALUES (%d, %d, %d, %f, %f, %f, %f, %f, %d, %f, %d, %f, %d, %d)",
+					compressedResponse.ContractID, compressedResponse.Entries.TotalTradeVolume.Size, compressedResponse.Entries.OpenInterest.Size, compressedResponse.Entries.OpeningPrice.Price, compressedResponse.Entries.HighPrice.Price, compressedResponse.Entries.SettlementPrice.Price,
+					compressedResponse.Entries.LowPrice.Price, compressedResponse.Entries.Bid.Price, compressedResponse.Entries.Bid.Size, compressedResponse.Entries.Offer.Price, compressedResponse.Entries.Offer.Size, compressedResponse.Entries.Trade.Price, compressedResponse.Entries.Trade.Size,
+					compressedResponse.Timestamp.UnixNano()/(1000*1000))
 				quoteIn, err := db.Prepare(statement)
 				if err != nil {
 					log.Println("Something went wrong preparing a SQL Insertion")
@@ -293,7 +293,7 @@ func main() {
 
 	ticker := time.NewTicker(time.Second)
 	defer ticker.Stop()
-	
+
 	go func() {
 		for {
 			select {
@@ -319,10 +319,9 @@ func main() {
 				}
 				return
 			}
-		}	
+		}
 	}()
 
-	
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", rootPage)
 	router.HandleFunc("/recent/", recentQuote)
@@ -330,47 +329,47 @@ func main() {
 	log.Fatal(http.ListenAndServe(":6009", router))
 }
 
-type HTTPTextResponse struct {
-	Code int `json:"code"`
+type httpTextResponse struct {
+	Code    int    `json:"code"`
 	Message string `json:"reason"`
 }
 
-type QuoteObject struct {
-	Timestamp int64 `json:"timestamp"`
-	ContractID int `json:"contract_id"`
-	SessionVolume int `json:"session_volume"`
-	OpenInterest int `json:"open_interest"`
-	SessionPrices SessionObject `json:"session_prices"`
-	Depth DepthObject `json:"depth"`
-	Trade PriceWithSize `json:"trade"`
+type quoteObjectStruct struct {
+	Timestamp     int64         `json:"timestamp"`
+	ContractID    int           `json:"contract_id"`
+	SessionVolume int           `json:"session_volume"`
+	OpenInterest  int           `json:"open_interest"`
+	SessionPrices sessionObject `json:"session_prices"`
+	Depth         depthObject   `json:"depth"`
+	Trade         priceWithSize `json:"trade"`
 }
 
 func loggingMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        // Do stuff here
-        log.Println("HTTP Request Received on", r.RequestURI)
-        // Call the next handler, which can be another middleware in the chain, or the final handler.
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Do stuff here
+		log.Println("HTTP Request Received on", r.RequestURI)
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(w, r)
+	})
 }
 
 func rootPage(w http.ResponseWriter, r *http.Request) {
-	response := HTTPTextResponse{
-		Code: 200,
+	response := httpTextResponse{
+		Code:    200,
 		Message: "You have reached the market data provisioner root endpoint, please see the documentation to access public facing endpoints.",
 	}
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(response)
 }
 
-type PayloadResponse struct {
-	Code int `json:"code"`
-	Payload QuoteObject `json:"payload"`
+type payloadResponse struct {
+	Code    int               `json:"code"`
+	Payload quoteObjectStruct `json:"payload"`
 }
 
 func recentQuote(w http.ResponseWriter, r *http.Request) {
-	response := PayloadResponse{
-		Code: 200,
+	response := payloadResponse{
+		Code:    200,
 		Payload: quoteObject,
 	}
 	w.WriteHeader(http.StatusOK)
